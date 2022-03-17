696c6fee24774488a9dd3ffb2815cfb85bc4b182
Linden Quan <linden.quan@resmed.com>
2022-03-17 13:15:33 -0300

    code review

--- src/ddbToS3/ddbToS3Archive.ts
+++ src/ddbToS3/ddbToS3Archive.ts
@@ -18,27 +18,30 @@ export class DdbToS3Archive {
     }
 
     async handleDDBStreamEvent(event: any) {
-        try {
-            if (event && event.Records) {
-                console.log(`DynamoDB stream published ${event.Records.length} records to process.`);
+        if (event && event.Records) {
+            console.log(`DynamoDB stream published ${event.Records.length} records to process.`);
 
-                // get records that were removed from TTL elapse
-                let records = utility.filterRemovedRecordsFromTTL(event.Records);
-                logger.info(`DynamoDB stream published ${records.length} records removed by TTL to process.`);
+            // get records that were removed from TTL elapse
+            let records = utility.filterRemovedRecordsFromTTL(event.Records);
+            logger.info(`DynamoDB stream published ${records.length} records removed by TTL to process.`);
+            try {
                 await this.firehose.putRecords(records);
-
-                // get records that need to be updated with TTL field
-                records = utility.filterRecordsNeedUpdateTTL(event.Records, ttlsInSeconds);
-                logger.info(
-                    `DynamoDB stream published ${records.length} records that need to be updated with TTL field.`,
-                );
-                await this.dynamodb.updateRecords(records, ttlsInSeconds);
-            } else {
-                logger.info('No records published by Dynamodb stream.');
+            } catch (e) {
+                logger.error('Failed to put records to Firehose', e);
+                throw e;
             }
-        } catch (e) {
-            logger.error('Failed to set TTL field or archive change records', e);
-            throw e;
+
+            // get records that need to be updated with TTL field
+            records = utility.filterRecordsNeedUpdateTTL(event.Records, ttlsInSeconds);
+            logger.info(`DynamoDB stream published ${records.length} records that need to be updated with TTL field.`);
+            try {
+                await this.dynamodb.updateRecords(records, ttlsInSeconds);
+            } catch (e) {
+                logger.error('Failed to update records with TTL fields', e);
+                throw e;
+            }
+        } else {
+            logger.info('No records published by Dynamodb stream.');
         }
     }
 }
